09d1647d1e736a04e1ae0c300aa3658038f53fde
ttuananh112 <t.tuananh112@gmail.com>
2019-03-12 10:27:06 +0700

    test 1 (car control in white line)

--- src/car_control.py
+++ src/car_control.py
@@ -4,12 +4,6 @@ import rospy
 from std_msgs.msg import Float32
 import cv2
 import math
-import time
-from time import sleep
-# import torchvision.transforms as transforms
-# from model import *
-from PIL import Image
-# import torch
 import rospkg
 import cv2
 import numpy as np
@@ -17,9 +11,7 @@ from model_keras import nvidia_model
 path = rospkg.RosPack().get_path('team105')
 from augmentation import read_img
 import glob
-# import test
 import keras
-# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 import tensorflow as tf
 print(tf.__version__)
@@ -30,114 +22,64 @@ config.gpu_options.per_process_gpu_memory_fraction = 0.4
 set_session(tf.Session(config=config))
 
 class car_control:
-    def __init__(self, team_name):
+    def __init__(self):
         rospy.init_node('team105', anonymous=True)
-        self.speed_pub = rospy.Publisher(team_name + "_speed", Float32, queue_size=1)
-        self.steerAngle_pub = rospy.Publisher(team_name + "_steerAngle", Float32, queue_size=1)
+        self.speed_pub = rospy.Publisher("/set_speed_car_api", Float32, queue_size=1)
+        self.steerAngle_pub = rospy.Publisher("/set_steer_car_api", Float32, queue_size=1)
         rospy.Rate(10)
 
-        # Load pytorch model
-        # self.net = NvidiaNet().to(device)
-        # self.net.load_state_dict(torch.load(path + '/param/model_yuv_1550497211.4968872_1.4583244496025145', map_location=device))
-
-        # self.transform = transforms.Compose([
-        #     transforms.Resize((100, 200)),
-        #     transforms.ToTensor()]
-        # )
-        self.i = 0
         # Load keras model
         self.model = nvidia_model()
         self.model.load_weights(path + '/param/cds-weights.09-0.02990.h5')
         self.model._make_predict_function()
-        # self.test(self.model)
-        # checkpoint = torch.load(path + '/param/model3.h5', map_location=lambda storage, loc: storage)
-        # self.net = checkpoint['net'].to(device)
+        self.h, self.w = 240, 320
         self.carPos = (160, 240)
         self.last_detected = 0
         self.sign_type = 0
         self.is_turning = False
+        # self.test(self.model)
 
-    def control(self, sign, middlePos, img):
+    def control(self, img, sign, isGo):
         if not rospy.is_shutdown():
-            now = time.time()
-            diff = now - self.last_detected
+            steerAngle = self.cal_steerAngle(img, sign) * 1.5
+            print(steerAngle, isGo)
 
-            steerAngle = self.cal_steerAngle(sign, middlePos, diff, img)
-            print(steerAngle)
+            speed = 12
+            # High steer angle
+            if math.fabs(steerAngle) >= 30:
+                speed = 12
 
-            speed = 50
-            # cant detect 2 lanes
-            if (middlePos[0] == -1 or math.fabs(steerAngle) >= 10) and diff > 2:
-                speed = 30
-            elif math.fabs(steerAngle) >= 5 or (diff > 0.1 and diff < 2):
-                speed = 40
+            if isGo == False:
+                steerAngle = 0
+                speed = 0
 
             # print(middlePos[0])
             self.speed_pub.publish(speed)
-            self.steerAngle_pub.publish(steerAngle)
+            self.steerAngle_pub.publish(-steerAngle)
 
         return self.is_turning, steerAngle, speed
 
 
-    def cal_steerAngle(self, sign, middlePos, diff, img):
-        carPos_x, carPos_y = self.carPos
-
-        middlePos_x, middlePos_y = middlePos
-
+    def cal_steerAngle(self, img, sign):
         steerAngle = 0
 
-        if (sign[4] > 20):
-            self.last_detected = time.time()
-            self.sign_type = 1
-        if (sign[4] < -20):
-            self.last_detected = time.time()
-            self.sign_type = -1
+        img_array = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img_bv = self.bird_view(img_array)
+        middle_pos = float(self.model.predict(img_bv[None, :, :, :], batch_size=1)) * 160 + 160
 
-        if (diff > 0.35 and diff < 0.5):
-            self.is_turning = True
+        if middle_pos > 320:
+            middle_pos = 320
+        if middle_pos < 0:
+            middle_pos = 0
 
-        if self.is_turning:
-            if diff < 1.5:
-                steerAngle = 50 * self.sign_type
-            else:
-                if middlePos_x == -1:
-                    self.last_detected = time.time() - 1.5
-                    steerAngle = 50 * self.sign_type
-                else:
-                    self.is_turning = False
-                    steerAngle = 0
-        elif middlePos_x == -1:
-            steerAngle = 0
-        else:
-            # print(middlePos_x, steerAngle)
+        cv2.line(img_bv, (int(middle_pos), self.h / 2), (self.w / 2, self.h), (255, 0, 0), 2)
+        cv2.imshow("Bird view", img_bv)
+        # Distance between MiddlePos and CarPos
+        distance_x = middle_pos - self.w / 2
+        distance_y = self.h - self.h / 2
 
-            # img = np.array(img, dtype='>f') / 255.
-            # img = cv2.resize(img[80:, :, :], (150, 200))
-            # img = img.reshape(1, 150, 200, 3).transpose((0, 3, 1, 2))
-                # img = torch.from_numpy(img).to(device)
-                # img = cv2.cvtColor(, (150, 200))
-            img_array = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            img_array = np.array(img_array)
-            # img = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
-            # img = Image.fromarray(img[80:, :, :])
-            # img = self.transform(img).to(device)
-            # img = img.view(1, 3, 100, 200)
-            # image = torch.autograd.Variable(img)
-
-            # middle_pos = self.net(img).view(-1).data.cpu().numpy()[0] * 320
-            # middle_pos = float(self.model.predict(img_array[None, :, :, :], batch_size=1)) * 80 + 160
-            middle_pos = float(self.model.predict(self.bird_view(img_array)[None, :, :, :], axis=0), batch_size=1) * 160 + 160
-            if middle_pos > 320:
-                middle_pos = 320
-            if middle_pos < 0:
-                middle_pos = 0
-            # Distance between MiddlePos and CarPos
-            distance_x = middle_pos - carPos_x
-            distance_y = carPos_y - middlePos_y
-
-            print(middlePos_x, middle_pos, middlePos_x - middle_pos)
-            # Angle to middle position
-            steerAngle = math.atan(distance_x / distance_y) * 180 / math.pi
+        # Angle to middle position
+        steerAngle = math.atan(float(distance_x) / distance_y) * 180 / math.pi
 
         return steerAngle
 
@@ -146,12 +88,10 @@ class car_control:
         for img_path in glob.glob(path + '/test/*.jpg'):
             test_img = read_img(img_path)
 
-            middle_pos = img_path.split('_')[-3]
+            middle_pos = float(img_path.split('_')[-2]) * 160 + 160
 
             test_img = np.array(test_img)
-            predicted_middle_pos = float(model.predict(self.bird_view(test_img)[None, :, :, :], batch_size=1)) * 160 + 160
-
-            # predicted_middle_pos = float(self.model.predict(np.expand_dims(test_img, axis=0), batch_size=1)) * 80 + 160
+            predicted_middle_pos = float(model.predict(test_img[None, :, :, :], batch_size=1)) * 160 + 160
 
             print(middle_pos, predicted_middle_pos)
 
--- src/team105_node.py
+++ src/team105_node.py
@@ -6,81 +6,51 @@ roslib.load_manifest('team105')
 import sys
 import rospy
 import cv2
-from std_msgs.msg import Float32
+from std_msgs.msg import Float32, Bool
 from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 import matplotlib.pyplot as plt
 from car_control import car_control
-from sign_detection import SignDetection
+# from sign_detection import SignDetection
 from lane_detector import lane_detector
-from object_detection import detect_object
 import time
-TEAM_NAME = 'team105'
 
 
 class image_converter:
     def __init__(self):
-        self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber(TEAM_NAME + "_image/compressed", CompressedImage,
-                                          callback=self.callback, queue_size=1)
-        self.cc = car_control(TEAM_NAME)
+        self.isGo = True
+        self.go_sub = rospy.Subscriber("/ss_status", Bool, callback=self.isAGo, queue_size=1)
+
+
+        self.cc = car_control()
         self.ld = lane_detector()
         self.curr_time = "100"
         rospy.Rate(10)
-        self.sd = SignDetection()
-        self.i = 0
+        # self.sd = SignDetection()
         self.is_turning = False
 
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("camera/rgb/image_raw/compressed", CompressedImage, callback=self.callback, queue_size=1)
+
+    def isAGo(self,data):
+        self.isGo = data.data
+
     def callback(self, data):
-        # print("callback")
         try:
             np_arr = np.fromstring(data.data, np.uint8)
             image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
             # NOTE: image_np.shape = (240,320,3)
-
-            out_img, sign = self.sd.detect_sign(image_np)
-            # out_img = image_np
-            # sign = (0, 0, 0, 0, 0)
-            # sign_size = 0
-            #cv2.imshow("Object", img_object)
-            #cv2.waitKey(1)
-
-            # image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
-            out_img, middlePos = self.ld.lane_detect(out_img, self.is_turning)
-            # print(middlePos)
-            # print("Left ",left_fit," Right ",right_fit)
-
-            cv2.imshow("Middle Pos", out_img)
-            #img_object = detect_object(out_img)
-            #cv2.imshow("Detect Object", img_object)
-            cv2.waitKey(1)
-
+            image_np = cv2.resize(image_np, (320, 240))
+            sign = 0
             # drive
-            if self.i == 0:
-                self.is_turning,steer_angle,speed = self.cc.control(sign, (middlePos[0], middlePos[2]), image_np)
-                self.i = 0
-            else:
-                self.i += 1
-            # milliseconds = int(round(time.time() * 1000))
-            #
-            # if int(self.curr_time) <= milliseconds:
-            #     file_name = "/home/nda97531/catkin_ws/src/team105/CDS/" + self.curr_time + "_"+ str(steer_angle)+"_"+ str(speed)+ ".jpg"
-            #     self.curr_time = str(milliseconds+300)
-            #     cv2.imwrite(file_name, image_np)
 
+            self.is_turning, steer_angle, speed = self.cc.control(image_np, sign, self.isGo)
+            
         except CvBridgeError as e:
             print(e)
 
 if __name__ == '__main__':
     rospy.init_node('team105', anonymous=True)
-    # cv2.namedWindow("houghlines")
-    # def nothing():
-    #     pass
-    # cv2.createTrackbar("rho", "houghlines",2,255,nothing)
-    # cv2.createTrackbar("theta", "houghlines",180,255,nothing)
-    # cv2.createTrackbar("minLine", "houghlines",78,255,nothing)
-    # cv2.createTrackbar("maxGap", "houghlines",10,255,nothing)
-    # cv2.waitKey(1)
     ic = image_converter()
     rospy.spin()
